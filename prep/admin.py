b06d9328eed1b40f557effb5534ba2ede82fbf80
Shaaz29 <ahmadshaaz2910@gmail.com>
2026-03-09 15:00:07 +0530

    Added django-import-export and updated admin panel for bulk question upload

--- prep/admin.py
+++ prep/admin.py
@@ -1,33 +1,30 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 from .models import Course, MockTest, Question, Result
 
 
-# ================= COURSE ADMIN =================
 @admin.register(Course)
-class CourseAdmin(admin.ModelAdmin):
+class CourseAdmin(ImportExportModelAdmin):
     list_display = ("id", "name")
     search_fields = ("name",)
 
 
-# ================= MOCKTEST ADMIN =================
 @admin.register(MockTest)
-class MockTestAdmin(admin.ModelAdmin):
+class MockTestAdmin(ImportExportModelAdmin):
     list_display = ("id", "course", "year", "duration")
     list_filter = ("course", "year")
     search_fields = ("course__name",)
 
 
-# ================= QUESTION ADMIN =================
 @admin.register(Question)
-class QuestionAdmin(admin.ModelAdmin):
+class QuestionAdmin(ImportExportModelAdmin):
     list_display = ("id", "mocktest", "type", "text", "correct_answer")
     list_filter = ("mocktest", "type")
     search_fields = ("text",)
 
 
-# ================= RESULT ADMIN =================
 @admin.register(Result)
-class ResultAdmin(admin.ModelAdmin):
+class ResultAdmin(ImportExportModelAdmin):
     list_display = ("id", "user", "mocktest", "score", "date")
     list_filter = ("mocktest", "date")
     search_fields = ("user__username",)
